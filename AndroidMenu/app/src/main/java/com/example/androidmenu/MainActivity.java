7966119e1b53a0b32cdd87e9ee48e59522ee2eaf
karajanov <karajanovb@yahoo.com>
2020-03-04 15:00:26 +0100

    menu options event handlers

--- AndroidMenu/app/src/main/java/com/example/androidmenu/MainActivity.java
+++ AndroidMenu/app/src/main/java/com/example/androidmenu/MainActivity.java
@@ -10,31 +10,35 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout mainlayout;
     boolean isDark = false;
-    TextView t;
-    Button b;
+    Button[] operatorBtns = new Button[6];
+    EditText firstOperand, secondOperand;
+    TextView result;
+    boolean isScientific = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialization
         mainlayout = findViewById(R.id.mainlayout);
-        b = findViewById(R.id.button_add);
-        t= findViewById(R.id.textViewResult);
+        firstOperand = findViewById(R.id.editTextOne);
+        secondOperand = findViewById(R.id.editTextTwo);
+        result = findViewById(R.id.textViewResult);
+        initializeOperatorBtns();
 
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        //Hide additional buttons
+         hideAdditionalBtns();
 
-                t.setText("changed");
+        //Event listeners
 
-            }
-        });
     }
 
     @Override
@@ -47,16 +51,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
 
             case R.id.item_darkmode:
 
-                if(isDark){
+                if (isDark) {
                     mainlayout.setBackgroundColor(Color.WHITE);
                     isDark = false;
-                }else{
+                } else {
                     mainlayout.setBackgroundColor(Color.LTGRAY);
                     isDark = true;
+                }
+
+                break;
+
+            case R.id.item_scientific:
+
+                if (isScientific) {
+
+                    item.setTitle("Scientific Mode");
+                    hideAdditionalBtns();
+                    isScientific = false;
+
+                } else {
+
+                    item.setTitle("Regular Mode");
+                    displayAdditionalBtns();
+                    isScientific = true;
+
                 }
 
                 break;
@@ -65,4 +87,69 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
+    private int getOperatorBtnId(int index) {
+
+        int opId = getResources()
+                .getIdentifier("button" + index, "id", getPackageName());
+
+        return opId;
+
+    }
+
+    private void initializeOperatorBtns() {
+
+        for (int i = 0; i < operatorBtns.length; ++i) {
+            int opId = getOperatorBtnId(i);
+            operatorBtns[i] = findViewById(opId);
+        }
+    }
+
+    private void hideAdditionalBtns() {
+        operatorBtns[4].setVisibility(View.GONE);
+        operatorBtns[5].setVisibility(View.GONE);
+    }
+
+    private void displayAdditionalBtns() {
+        operatorBtns[4].setVisibility(View.VISIBLE);
+        operatorBtns[5].setVisibility(View.VISIBLE);
+    }
+
+    private void addOperatorEventHandlers(){
+
+        for(int i = 0; i < operatorBtns.length; ++i){
+
+            final int tempI = i;
+
+            operatorBtns[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    switch(operatorBtns[tempI].getText().toString()){
+
+                        case "+":
+                            break;
+
+                        case "-":
+                            break;
+
+                        case "*":
+                            break;
+
+                        case "/":
+                            break;
+
+                        case "^":
+                            break;
+
+                        case "sqrt":
+                            break;
+                    }
+                }
+            });
+
+        }
+
+    }
+
 }
